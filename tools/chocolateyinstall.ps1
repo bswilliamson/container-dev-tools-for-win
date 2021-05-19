64b0795701a5cef6dca9c3ceec4859763167207a
bswilliamson <bswilliamson>
2021-05-19 19:36:12 +0100

    Install extensions for correct user

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -5,11 +5,21 @@ $ErrorActionPreference = 'Stop'
 #
 
 Function Add-VSCodeExtension {
-  Param($name)
+  Param($name, $user)
+  Write-Verbose "installing: $name"
+  $userHome = "$env:SystemDrive\Users\$user"
+  $userExtDir = "$userHome\.vscode\extensions"
+  $userDataDir = "$userHome\AppData\Roaming\Code"
+  Write-Debug "vscode ext: http_proxy=$env:http_proxy"
+  Write-Debug "vscode ext: https_proxy=$env:https_proxy"
+  Write-Debug "vscode ext: name=$name"
+  Write-Debug "vscode ext: extDir=$userExtDir"
+  Write-Debug "vscode ext: dataDir=$userDataDir"
   try {
-    code --install-extension $name
+    code --install-extension $name --extensions-dir $userExtDir --user-data-dir $userDataDir --force
   } catch {
-    # todo: handle buffer warning error
+    # todo: handle buffer warning error, fail for anything else
+    Write-Debug $_
   }
 }
 
@@ -29,11 +39,12 @@ if ($user -eq $null) {
 
 Write-Output 'configuring: vscode'
 # vscode seems to ignore the system proxy when invoked from here so we'll set it manually
-$env:http_proxy = $http_proxy
-$env:https_proxy =  $https_proxy
-Add-VSCodeExtension 'ms-vscode-remote.remote-wsl'
-Add-VSCodeExtension 'ms-vscode-renote.remote-containers'
-Add-VSCodeExtension 'ms-azuretools.vscode-docker'
+$env:http_proxy = $httpProxy
+$env:https_proxy =  $httpsProxy
+
+Add-VSCodeExtension -User $user -Name 'ms-vscode-remote.remote-wsl'
+Add-VSCodeExtension -User $user -Name 'ms-vscode-renote.remote-containers'
+Add-VSCodeExtension -User $user -Name 'ms-azuretools.vscode-docker'
 
 Write-Output 'configuring: docker'
 Add-LocalGroupMember -Group "docker-users" -Member $user -ea 0
