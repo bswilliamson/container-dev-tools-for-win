445cdcea39d141cc46bae40d671b7599eba6d78f
bswilliamson <49656267+bswilliamson@users.noreply.github.com>
2021-05-19 15:07:50 +0100

    Added install script for testing

---
+++ install.ps1
@@ -0,0 +1,16 @@
+param($Debug)
+param($Verbose)
+$ErrorActionPreference = 'Stop'
+
+choco pack
+
+$installArgs = '-fy'
+if ($Debug -eq $true) {
+  $installArgs += 'd'
+}
+if ($Verbose -eq $true) {
+   $installArgs += 'v'
+}
+choco install $installArgs container-dev-tools-for-win -s "'.;https://chocolatey.org/api/v2'" --Params "/User:User"
+
+rm *.nupkg
